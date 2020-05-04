24a8500057dd9da6012ee6508e64c48e75340a08
dzinsyah <dzinsyah@alterra.id>
2020-05-04 12:52:02 +0700

    Updates

---
+++ precache-manifest.7dc388580a19a3bea4ba2d1386e584c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9040d34c6d6b7794b3f98ea94316e1e",
+    "url": "/React-Alta-Batch5/index.html"
+  },
+  {
+    "revision": "3e7758b57ded48bf3645",
+    "url": "/React-Alta-Batch5/static/css/main.f5d3d956.chunk.css"
+  },
+  {
+    "revision": "290ab1969c5823887cea",
+    "url": "/React-Alta-Batch5/static/js/2.b7086350.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Alta-Batch5/static/js/2.b7086350.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e7758b57ded48bf3645",
+    "url": "/React-Alta-Batch5/static/js/main.f19158da.chunk.js"
+  },
+  {
+    "revision": "e06b71269f91e4b8e987",
+    "url": "/React-Alta-Batch5/static/js/runtime-main.6516d8d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Alta-Batch5/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0164152affe834feff9751999eae8281.js"
+  "/React-Alta-Batch5/precache-manifest.7dc388580a19a3bea4ba2d1386e584c9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Alta-Batch5/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
