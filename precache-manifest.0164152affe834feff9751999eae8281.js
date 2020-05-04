8a78c4e4f83bc4467c40f9e58aa708b8a9ce26e2
dzinsyah <dzinsyah@alterra.id>
2020-05-04 12:48:18 +0700

    Updates

---
+++ precache-manifest.0164152affe834feff9751999eae8281.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68c7cc297e208e8e0dc6d16d25007bec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d307cd0fc4f27667ae8a",
+    "url": "/static/css/main.f5d3d956.chunk.css"
+  },
+  {
+    "revision": "290ab1969c5823887cea",
+    "url": "/static/js/2.b7086350.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b7086350.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d307cd0fc4f27667ae8a",
+    "url": "/static/js/main.97e94b93.chunk.js"
+  },
+  {
+    "revision": "b769de6bf54d6d4ed519",
+    "url": "/static/js/runtime-main.dc7fc8ac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
